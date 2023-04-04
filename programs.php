3f46c6317c10b47d89fec534bea1f84a4bbbb526
Thaniyell <essien.nathan@yahoo.com>
2023-04-04 20:14:26 +0100

    Updated footer styles and added quote to all pages

--- programs.php
+++ programs.php
@@ -4,6 +4,22 @@ $css_file = '<link rel="stylesheet" href="css/programs.css">';
 echo '<script defer src="./js/home.js"></script>';
 include('includes/header.php'); ?>
 
+<!-- Font Awesome -->
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+  rel="stylesheet"
+/>
+<!-- Google Fonts -->
+<link
+  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+  rel="stylesheet"
+/>
+<!-- MDB -->
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.2.0/mdb.min.css"
+  rel="stylesheet"
+/>
+
 <div class="wrapper">
   <div class="section1">
     <div class="left-side">
@@ -176,7 +192,86 @@ include('includes/header.php'); ?>
             </form>
         </div>
 </div>
+ <!-- testimonial section -->
+    <!-- Carousel wrapper -->
+    <div id="carouselExampleControls" class="carousel slide text-center carousel-dark mb-5" data-mdb-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <div class="row d-flex justify-content-center">
+                <div class="col-lg-8">
+                <h5 class="mb-3">Funke Opeke</h5>
+                <p> Founder and CEO of MainOne</p>
+                <p class="text-muted px-3">
+                    <i class="fas fa-quote-left pe-2"></i>
+                    Technology can bridge the gap in healthcare, education, and economic development in Africa.
+                    <i class="fas fa-quote-right pe-2"></i>
+                </p>
+                </div>
+            </div>
+            <!-- <ul class="list-unstyled d-flex justify-content-center text-warning mb-0">
+                <li><i class="fas fa-star fa-sm"></i></li>
+                <li><i class="fas fa-star fa-sm"></i></li>
+                <li><i class="fas fa-star fa-sm"></i></li>
+                <li><i class="fas fa-star fa-sm"></i></li>
+                <li><i class="far fa-star fa-sm"></i></li>
+            </ul> -->
+        </div>
+        <div class="carousel-item">
+            <div class="row d-flex justify-content-center">
+                <div class="col-lg-8">
+                <h5 class="mb-3">Dr. Naledi Pandor</h5>
+                <p> South African Minister of Higher Education and Training</p>
+                <p class="text-muted px-3">
+                    <i class="fas fa-quote-left pe-2"></i>
+                    Africa needs to invest in digital infrastructure and the skills of its people to take advantage of the fourth industrial revolution.
+                    <i class="fas fa-quote-right pe-2"></i>
+                </p>
+                </div>
+            </div>
+            <!-- <ul class="list-unstyled d-flex justify-content-center text-warning mb-0">
+                <li><i class="fas fa-star fa-sm"></i></li>
+                <li><i class="fas fa-star fa-sm"></i></li>
+                <li><i class="fas fa-star fa-sm"></i></li>
+                <li><i class="fas fa-star fa-sm"></i></li>
+                <li><i class="far fa-star fa-sm"></i></li>
+            </ul> -->
+        </div>
+        <div class="carousel-item">
+            <div class="row d-flex justify-content-center">
+                <div class="col-lg-8">
+                <h5 class="mb-3">Baratang Miya</h5>
+                <p>South African social entrepreneur and founder of Girlhype Coders</p>
+                <p class="text-muted px-3">
+                    <i class="fas fa-quote-left pe-2"></i>
+                    Innovation is about creating solutions that are accessible, affordable, and sustainable for everyone, not just a privileged few.
+                    <i class="fas fa-quote-right pe-2"></i>
+                </p>
+                </div>
+            </div>
+            <!-- <ul class="list-unstyled d-flex justify-content-center text-warning mb-0">
+                <li><i class="fas fa-star fa-sm"></i></li>
+                <li><i class="fas fa-star fa-sm"></i></li>
+                <li><i class="fas fa-star fa-sm"></i></li>
+                <li><i class="fas fa-star fa-sm"></i></li>
+                <li><i class="far fa-star fa-sm"></i></li>
+            </ul> -->
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-mdb-target="#carouselExampleControls"
+        data-mdb-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-mdb-target="#carouselExampleControls"
+        data-mdb-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+    </div>
 
+    <script
+    type="text/javascript"
+    src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.2.0/mdb.min.js"></script>
 <?php
 include('includes/footer.php')
 
